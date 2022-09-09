ec1b2ce194c52a398ce72651a1673a40136696bc
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-09-09 23:18:09 +0700

    make billing scetion but unperfect

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
 
-const Billing = () => {
-  return <div>Billing</div>;
-};
+const Billing = () => (
+  <section id="product" className={`${layout.sectionReverse}`}>
+    <div className={`${layout.sectionImgReverse}`}>
+      <img src="" alt="slider" className="w-[100%] h-[100%] relative z-5" />
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+    </div>
+    <div className={`${layout.sectionInfo}`}>
+      <h2 className={`${styles.heading2}`}>
+        Lorem ipsum dolor sit amet. <br className="sm:block hidden" /> Lorem, ipsum dolor.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore blanditiis excepturi ad numquam eveniet deserunt molestiae expedita voluptas rerum porro!</p>
+    </div>
+    <div></div>
+  </section>
+);
 
 export default Billing;
