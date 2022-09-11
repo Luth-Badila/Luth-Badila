db3877ab27114bdf05f7a1c6f130d9e254295033
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-09-11 22:40:53 +0700

    no more added just try change background and cta button and getsatrted

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -3,7 +3,7 @@ import styles from "../style";
 import { arrowUp } from "../assets";
 
 const GetStarted = () => (
-  <div className={`${styles.flexCenter} w-[120px] h-[120px] rounded-full bg-red-gradient p-[2px] cursor-pointer hover:scale-110`}>
+  <div className={`${styles.flexCenter} w-[120px] h-[120px] rounded-full bg-red-gradient p-[3px] cursor-pointer hover:scale-110 hover:p-[4px]`}>
     <div className={`${styles.flexCenter} flex-col bg-red w-[100%] h-[100%] rounded-full`}>
       <div className={`${styles.flexStart} flex-row`}>
         <a href="https://wa.me/087856524677" target="__blank" className="font-poppins font-medium text-[18px] leading-[23px] cursor-pointer mr-2">
