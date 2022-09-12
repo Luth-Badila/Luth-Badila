bf3940286cc128512999f974ef05750539d54d04
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-09-12 10:45:42 +0700

    make perfect portofolio website but need some image and animation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,15 +12,24 @@ const Footer = () => (
       <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
         {footerLinks.map((footerLink) => (
           <div key={footerLink.key} className="flex flex-col ss:my-0 my-4 min-w-[150px]">
-            <h4>{footerLink.title}</h4>
-            <ul>
+            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">{footerLink.title}</h4>
+            <ul className="list-none mt-4">
               {footerLink.links.map((link, index) => (
-                <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer`}>
+                <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== footerLink.links.length - 1 ? "mb-4" : "mb-0"}`}>
                   {link.name}
                 </li>
               ))}
             </ul>
           </div>
+        ))}
+      </div>
+    </div>
+
+    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-[#8f2d00]">
+      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">2022 Luth-Badila. All Right Reserved</p>
+      <div className="flex flex-row md:mt-0 mt-6">
+        {socialMedia.map((socmed, index) => (
+          <img key={socmed.id} src={socmed.icon} alt={socmed.id} className={`w-[21px] h-[21px]object-contain cursor-pointer ${index !== socialMedia.length - 1 ? "mr-6" : "mr-0"}`} />
         ))}
       </div>
     </div>
