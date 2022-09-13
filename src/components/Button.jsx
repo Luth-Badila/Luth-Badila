9a4f7f993e28537cc9d815df0f43ebfefa69dc3e
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-09-13 11:22:40 +0700

    added whilehover in button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Button = ({ styles }) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-red-gradient font-poppins font-medium text-[18px] text-white outline-none ${styles} rounded-[10px] hover:scale-110 hover:text-dimWhite`}>
+    <motion.button type="button" className={`py-4 px-6 bg-red-gradient font-poppins font-medium text-[18px] text-white outline-none ${styles} rounded-[10px]`} whileHover={{ scale: 1.1, boxShadow: "0px 0px 8px rgb(255,255,255)" }}>
       order now
-    </button>
+    </motion.button>
   );
 };
 
