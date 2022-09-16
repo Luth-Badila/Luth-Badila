7fd6bb1073695b49c42d88ad64524c65a8efd81e
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-09-16 20:54:00 +0700

    added automatic slider

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,11 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 
 import styles from "../style";
 
+let slideInterval;
 const ImageSlider = ({ Slides }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const length = Slides.length;
+  const slideRef = useRef();
+  const removeAnimation = () => {
+    slideRef.current.classList.remove("fade-anim");
+  };
+  const pauseSlider = () => {
+    clearInterval(slideInterval);
+  };
+  const startSlider = () => {
+    slideInterval = setInterval(() => {
+      handleOnNextClick();
+    }, 3000);
+  };
+
+  useEffect(() => {
+    slideRef.current.addEventListener("animationend", removeAnimation);
+    slideRef.current.addEventListener("mouseenter", pauseSlider);
+    slideRef.current.addEventListener("mouseleave", startSlider);
+
+    startSlider();
+    return () => {
+      pauseSlider();
+    };
+    // eslint-disable-next-line
+  }, []);
+
   const nextSlide = () => {
     setCurrentIndex(currentIndex === length - 1 ? 0 : currentIndex + 1);
   };
@@ -18,7 +44,7 @@ const ImageSlider = ({ Slides }) => {
   }
   return (
     <>
-      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+      <div ref={slideRef} className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
         {Slides.map((slide, i) => {
           return (
             <div key={i} className={i === currentIndex ? "opacity-[1] ease-in duration-1000" : "opacity-0"}>
@@ -28,13 +54,6 @@ const ImageSlider = ({ Slides }) => {
             </div>
           );
         })}
-        {/* {Slides.map((slide, index) => (
-          <div className="relative" key={index}>
-            <FaArrowCircleRight size={35} className="absolute top-[50%] rounded-full right-[90px] text-white/70 hover:text-white cursor-pointer select-none z-[100] hover:text-red-700 p-2" />
-            {index === currentIndex && <img className="lg:w-[90%] lg:h-[90%] lg:px-0 px-4 w-[100%] h-[100%] relative z-[5]" src={slide[currentIndex]?.url} />}
-            <FaArrowCircleLeft size={35} className="absolute top-[50%] left-[20px] hover:text-white rounded-full text-white/70 cursor-pointer select-none z-[100] hover:text-red-700 p-2" />
-          </div>
-        ))} */}
       </div>
     </>
   );
