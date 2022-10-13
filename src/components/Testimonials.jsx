2a99507adc946bfed14fac2dbf9b5e30d74b5563
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-10-13 12:49:45 +0700

    fixed width in testimonial

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,16 +5,19 @@ import FeedbackCard from "./FeedbackCard";
 
 const Testimonials = () => {
   return (
-    <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative p-8`}>
+    <section className={`flex flex-col justify-center items-center px-10`}>
       {/* Gradient */}
-      <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full red__gradient" />
+      <div className="absolute z-[0] w-[60%] h-[60%] rounded-full red__gradient" />
       <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
-        <h1 className={styles.heading2}>
+        <h1 className={`${styles.heading2}`}>
           Testimoials from
           <br className="sm:block hidden" /> our clients
         </h1>
         <div className="w-full md:mt-0 mt-6">
-          <p className={`${styles.paragraph} text-left max-w-[450px]`}>clients who have created a website for their business clients who have created a website for their business</p>
+          <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+            clients who have created a website for their business clients who have created a website for their business clients who have created a website for their business clients who have created a website for their business clients who
+            have created a website for their business clients who have created a website for their business
+          </p>
         </div>
       </div>
       <div className="flex flex-wrap justify-start sm:justify-center w-full feedback-container relative z-[1]">
