e94888808358eac858904c62857605475e6964f6
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-10-13 12:20:17 +0700

    added supabase

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import { telegram1, instagram1, whatsapp1 } from "../assets";
+import supabase from "../config/supabaseClient";
 
 const Contact = () => {
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [note, setNote] = useState("");
+  const [formError, setFormError] = useState(null);
+  const handleSubmit = () => {};
+
   return (
     <>
       <div className="sm:h-[120vh] h-[140vh] flex flex-col justify-center items-center pb-4 sm:pb-6">
         <h2 className="text-white text-4xl">Contact Us</h2>
         <form className="flex flex-col gap-3 sm:w-[700px] w-[350px]">
           <label className="text-[20px] text-white">Subject</label>
-          <input type="text" required className="h-[50px] border border-black p-2 rounded-lg" />
+          <input type="text" value={subject} onChange={(e) => setSubject(e.target.value)} required className="h-[50px] border border-black p-2 rounded-lg" />
           <label className="text-[20px] text-white">Message</label>
-          <textarea required className="h-[100px] border border-black p-2 rounded-lg" />
+          <textarea value={message} onChange={(e) => setMessage(e.target.value)} required className="h-[100px] border border-black p-2 rounded-lg" />
           <label className="text-[20px] text-white">Web Type :</label>
           <select className="p-2 rounded-lg">
             <option>Portofolio</option>
@@ -24,8 +32,9 @@ const Contact = () => {
             <option>JSON Server</option>
           </select>
           <label className="text-[20px] text-white">Note :</label>
-          <input type="text" required className="h-[50px] border border-black p-2 rounded-lg" />
+          <input type="text" value={note} onChange={(e) => setNote(e.target.value)} required className="h-[50px] border border-black p-2 rounded-lg" />
           <button className="bg-red-gradient text-white rounded-lg cursor-pointer w-[130px] p-3">Send Message</button>
+          {formError && <p className="error">{formError}</p>}
         </form>
         <h2 className="text-white text-2xl mb-5 mt-3">Or</h2>
         <div className="flex flex-col gap-8 sm:flex-row">
